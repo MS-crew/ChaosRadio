a75c776047c8430146b7a022407fbd1ad4be643c
Mustafa SAVAŞ <mustafavesavas1@gmail.com>
2025-01-25 00:35:11 +0300

    more IL predisposed code

--- RadioTuning.cs
+++ RadioTuning.cs
@@ -8,6 +8,8 @@ using System.Reflection.Emit;
 using Exiled.API.Features.Pools;
 using Exiled.API.Features.Items;
 using System.Collections.Generic;
+using InventorySystem;
+using InventorySystem.Items;
 
 namespace ChaosRadio
 {
@@ -79,6 +81,74 @@ namespace ChaosRadio
             public static bool KaosTelsizAlan(ReferenceHub hub)
             {
                 return !hub.inventory.UserInventory.Items.Values.Any(item => KaosTelsiz.telsiz.Check(Item.Get(item)));
+            }
+        }
+    }
+    [HarmonyPatch(typeof(VoiceTransceiver), nameof(VoiceTransceiver.ServerReceiveMessage))]
+    public static class ServerReceiveMessageTranspilersq
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> Yenikodlar = ListPool<CodeInstruction>.Pool.Get(instructions);
+
+            LocalBuilder Kaostelsizmi = generator.DeclareLocal(typeof(bool));
+
+            Label Atla = generator.DefineLabel();
+            Label Atla2 = generator.DefineLabel();
+            Label Donguatla = generator.DefineLabel();
+
+            int GonderenKontrol = Yenikodlar.FindIndex(instruction => instruction.opcode == OpCodes.Call && instruction.operand.ToString().Contains("get_AllHubs"));
+
+            Yenikodlar[GonderenKontrol].labels.Add(Atla2);
+            Yenikodlar.InsertRange(GonderenKontrol, new List<CodeInstruction>
+            {
+                new CodeInstruction(OpCodes.Ldloc, 1),
+                new CodeInstruction(OpCodes.Ldc_I4, 2),
+                new CodeInstruction(OpCodes.Bne_Un, Atla2),
+
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(NetworkConnection), nameof(NetworkConnection.identity))),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(NetworkIdentity), nameof(NetworkIdentity.gameObject))),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(ReferenceHub), nameof(ReferenceHub.GetHub),[typeof(GameObject)])),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(ReferenceHub), nameof(ReferenceHub.inventory))),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(Inventory), nameof(Inventory.UserInventory))),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(InventoryInfo), nameof(InventoryInfo.Items))),
+                new CodeInstruction(OpCodes.Call,  AccessTools.Method(typeof(Kontrol), nameof(Kontrol.KaosTelsizGonderen))),
+                new CodeInstruction(OpCodes.Stloc, Kaostelsizmi.LocalIndex)
+            });
+
+            int Alankontrol = Yenikodlar.FindIndex(instruction => instruction.opcode == OpCodes.Ldarga_S);
+            Yenikodlar[Alankontrol].labels.Add(Atla);
+            Yenikodlar.InsertRange(Alankontrol, new List<CodeInstruction>
+            {
+                new CodeInstruction(OpCodes.Ldloc, 5),
+                new CodeInstruction(OpCodes.Ldc_I4, 2),
+                new CodeInstruction(OpCodes.Bne_Un, Atla),
+
+                new CodeInstruction(OpCodes.Ldloc_3),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(ReferenceHub), nameof(ReferenceHub.inventory))),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(Inventory), nameof(Inventory.UserInventory))),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.PropertyGetter(typeof(InventoryInfo), nameof(InventoryInfo.Items))),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Kontrol), nameof(Kontrol.KaosTelsizGonderen))),
+                new CodeInstruction(OpCodes.Ldloc, Kaostelsizmi.LocalIndex),
+                new CodeInstruction(OpCodes.And),
+                new CodeInstruction(OpCodes.Brfalse_S, Donguatla),
+            });
+
+            int DonguDevam = Yenikodlar.FindIndex(instruction => instruction.opcode == OpCodes.Call && instruction.operand is MethodInfo method4 && method4.Name == "MoveNext");
+            Yenikodlar[DonguDevam - 1].labels.Add(Donguatla);
+
+            for (int z = 0; z < Yenikodlar.Count; z++)
+                yield return Yenikodlar[z];
+
+            ListPool<CodeInstruction>.Pool.Return(Yenikodlar);
+        }
+
+        public static class Kontrol
+        {
+            public static bool KaosTelsizGonderen(Dictionary<ushort,ItemBase> Items)
+            {
+                return Items.Values.Any(item => KaosTelsiz.telsiz.Check(Item.Get(item)));
             }
         }
     }
